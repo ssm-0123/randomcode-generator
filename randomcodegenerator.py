c6963f02b70d3f9ca46ad46b3151625c9154a51e
ssm-0123 <ssm-0123@users.noreply.github.com>
2021-12-02 14:56:22 -0800

    Initial commit

---
+++ randomcodegenerator.py
@@ -0,0 +1,61 @@
+from json.encoder import py_encode_basestring_ascii
+import random
+import json
+
+letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
+
+def codes():
+    a = random.choice(letters)
+    b = random.choice(letters)
+    c = random.choice(letters)
+    d = random.choice(letters)
+    return a+b+c+d
+
+def makecode():
+    fromdb = open('db.txt')
+    db = fromdb.read()
+    listedcoupon = json.loads(db)
+    fromdb.close()
+    rancode = codes()
+    while True:
+        if rancode in listedcoupon:
+            pass
+        else:
+            listedcoupon.append(rancode)
+            print("Your code is "+rancode)
+            makedb = open('db.txt','w')
+            makedb.write(json.dumps(listedcoupon))
+            makedb.close()
+        break
+
+def usecode():
+    fromdb = open('db.txt')
+    db = fromdb.read()
+    listedcoupon = json.loads(db)
+    fromdb.close()
+    enteredcode = input("Enter the code ")
+    if enteredcode in listedcoupon:
+        listedcoupon.remove(enteredcode)
+        print("You used "+ enteredcode)
+        makedb = open('db.txt','w')
+        makedb.write(json.dumps(listedcoupon))
+        makedb.close()
+    else:
+        print("You put the wrong code")
+        
+def main():
+    while True:
+        print("1. Make a code")
+        print("2. Use a code")
+        print("3. Exit")
+        pick = input("")
+        if pick == "3":
+            break
+        elif pick == "2":
+            usecode()
+        elif pick == "1":
+            makecode()
+
+main()
+    
+
---
+++ test.py
@@ -0,0 +1,81 @@
+from json.encoder import py_encode_basestring_ascii
+import random
+import json
+
+letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
+
+def codes():
+    a = random.choice(letters)
+    b = random.choice(letters)
+    c = random.choice(letters)
+    d = random.choice(letters)
+    return a+b+c+d
+
+def makecode():
+    fromdb = open('db2.txt')
+    db = fromdb.read()
+    listedcoupon = json.loads(db)
+    fromdb.close()
+    rancode = codes()
+    while True:
+        if rancode in listedcoupon:
+            print(rancode+" is already exist")
+            pass
+        else:
+            listedcoupon.append(rancode)
+            print("Your code is "+rancode)
+            makedb = open('db2.txt','w')
+            makedb.write(json.dumps(listedcoupon))
+            makedb.close()
+        break
+
+def make100code():
+    while True:
+        fromdb = open('db2.txt')
+        db = fromdb.read()
+        listedcoupon = json.loads(db)
+        fromdb.close()
+        rancode = codes()
+        if len(listedcoupon)==100:
+            break
+        while True:
+            if rancode in listedcoupon:
+                print(rancode+" is already exist")
+                pass
+            else:
+                listedcoupon.append(rancode)
+                print("Your code is "+rancode)
+                makedb = open('db2.txt','w')
+                makedb.write(json.dumps(listedcoupon))
+                makedb.close()
+            break
+        
+def hackcode():
+    attempt = 0
+    while True:
+        fromdb = open('db2.txt')
+        db = fromdb.read()
+        listedcoupon = json.loads(db)
+        fromdb.close()
+        enteredcode = codes()
+        if enteredcode in listedcoupon:
+            listedcoupon.remove(enteredcode)
+            print("You used "+ enteredcode)
+            makedb = open('db2.txt','w')
+            makedb.write(json.dumps(listedcoupon))
+            makedb.close()
+            print(attempt)
+            break
+        else:
+            attempt = attempt + 1
+
+def howmanycode():
+    fromdb = open('db2.txt')
+    db = fromdb.read()
+    listedcoupon = json.loads(db)
+    print(len(listedcoupon))
+
+make100code()
+howmanycode()
+hackcode()
+howmanycode()
